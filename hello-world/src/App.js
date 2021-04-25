9bc0aceda66bddafa4143b963f64202ee1ccf444
Yashasree Tummala <yashasreetummala1991@gmail.com>
2021-04-25 12:26:20 +0530

    [Sahith | Pavan | Yash] Add. usestate() , callback functions with simple counter

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,12 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import Greet from './components/Greet'
-import Message from './components/Message';
+import Counter from './components/Counter';
 
 function App() {
   return (
     <div className="App">
-     <Message />
+     <Counter />
     </div>
   );
 }
--- hello-world/src/components/Message.js
+++ hello-world/src/components/Message.js
@@ -1,30 +1,25 @@
 import React, { Component } from "react";
 
 class Message extends Component {
+  constructor() {
+    super();
+    this.state = {
+      message: "Welcome Change",
+    };
+  }
 
-    constructor() {
-        super()
-        this.state = {
-            message : 'Welcome Change'
-        }
-    }
-
-
-    increment() {
-        this.setState(
-            {
-                message : 'Thanks for Subscribing'
-            }
-        )
-    }
+  increment() {
+    this.setState({
+      message: "Thanks for Subscribing"
+    });
+    console.log(this.props.name)
+  }
 
   render() {
     return (
       <div>
-        <h1>
-            {this.state.message}
-        </h1>
-        <button onClick= { () => increment()} >Subscribe</button>
+        <h1>{this.state.message}</h1>
+        <button onClick={() => this.increment()}>Subscribe</button>
       </div>
     );
   }
