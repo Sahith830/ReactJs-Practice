2eff83bfa0c1b07129cf66f2abad3f81d3a70b3a
Yashasree Tummala <yashasreetummala1991@gmail.com>
2021-04-25 12:03:50 +0530

    [Sahith | Pavan | Yash] Add. state & setState() implementation

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,28 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Greet from './components/Greet'
-import Welcome from './components/Welcome';
+import Message from './components/Message';
 
 function App() {
   return (
     <div className="App">
-      <Greet name = "Swara" age = "5">  
-        <p> Child Property </p>
-       </Greet> 
-      <Greet name = "Sahith" age = "15">
-        <button>Sahith</button>
-         </Greet>
-      <Greet name = "Pavan" age = "20"> 
-        <hr />
-       </Greet>
-
-       <Welcome name = "Swara" age = "5">  
-        <p> Child Property </p>
-       </Welcome> 
-      <Welcome name = "Sahith" age = "15">
-        <button>Sahith</button>
-         </Welcome>
-      <Welcome name = "Pavan" age = "20" /> 
+     <Message />
     </div>
   );
 }
