1d38471dbad2464738c19d9f1455947e0388d524
Yashasree Tummala <yashasreetummala1991@gmail.com>
2021-04-25 18:52:28 +0530

    [Pavan | Sahith | Yash] Add. Rendering list of items2 using map

--- hello-world/src/components/NameList.js
+++ hello-world/src/components/NameList.js
@@ -1,18 +1,20 @@
-import React from 'react'
+import React from "react";
 
 function NameList() {
-    const names = ['Sahith', 'Pavan', 'Yash']
-    // return (
-    //     <div>
-    //         {
-    //             names.map(name => <h2>{name}</h2>)
-    //         }
-    //     </div>
-    // )
-    // console.log('this can be re written to')
-
-    const NameList = names.map(name => <h2>{name}</h2>)
-    return <div>{NameList}</div>
+  const personsData = [
+    {
+      firstName: "Chaitanya",
+      age: 20
+    },
+    {
+      firstName: "Yashasree",
+      age: 21
+    }
+  ]
+  const personsList = personsData.map(person => (
+    <h2>{person.firstName} is {person.age} years old</h2>
+  ))
+  return <div>{personsList}</div>;
 }
 
-export default NameList
+export default NameList;
