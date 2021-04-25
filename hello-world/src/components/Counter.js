69ccab2f3db49c4c94801f8ccc743d829fb43dc3
Yashasree Tummala <yashasreetummala1991@gmail.com>
2021-04-25 12:36:59 +0530

    [Sahith | Pavan | Yash] Add. Incremneted counter using previous State

--- hello-world/src/components/Counter.js
+++ hello-world/src/components/Counter.js
@@ -4,28 +4,45 @@ class Counter extends Component {
   constructor() {
     super();
     this.state = {
-      count: 0
+      count: 0,
     };
   }
 
   increment() {
-    this.setState({
-      count : this.state.count + 1
-    }, () => {
+    this.setState((prevState) => (
+        {
+            count: prevState.count + 1
+        }
+    ), () => {
         console.log(this.state.count)
-    });
+    })
     
+  }
+
+  incrementFive() {
+      this.increment()
+      console.log(this.state.count)
+
+      this.increment()
+      console.log(this.state.count)
+
+      this.increment()
+      console.log(this.state.count)
+
+      this.increment()
+      console.log(this.state.count)
+
+      this.increment()
   }
 
   render() {
     return (
       <div>
         <h1>{this.state.count}</h1>
-        <button onClick={() => this.increment()}>Increment</button>
+        <button onClick={() => this.incrementFive()}>Increment</button>
       </div>
     );
   }
 }
 
-export default Counter
-;
+export default Counter;
