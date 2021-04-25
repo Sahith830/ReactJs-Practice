7592554cb769eeda2dd72544c5b083bd9d6673c6
Yashasree Tummala <yashasreetummala1991@gmail.com>
2021-04-25 18:34:55 +0530

    [Pavan | Sahith | Yash] Add. Conditional Rendering - Element Variables

--- hello-world/src/components/UserGreeting.js
+++ hello-world/src/components/UserGreeting.js
@@ -1,22 +1,23 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export class UserGreeting extends Component {
-    constructor(props) {
-        super(props)
-    
-        this.state = {
-             isLoggedIn : false
-        }
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoggedIn: false,
+    };
+  }
+
+  render() {
+    let message;
+    if (this.state.isLoggedIn) {
+      message = <div>Welcome Yash !</div>;
+    } else {
+      message = <div>Welcome Pavan !</div>;
     }
-    
-    render() {
-        if(this.state.isLoggedIn) {
-            return <div>Welcome Yash !</div>
-        }
-        else {
-            return <div>Welcome Sahith !</div>
-        } 
-    }
+    return <div>{message}</div>;
+  }
 }
 
-export default UserGreeting
+export default UserGreeting;
